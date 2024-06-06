d4eb1cd8ec63b4d515e7577e26aee88e8a34cb7c
Adhiraj Lamichhane <adhirajlamichhane1@gmail.com>
2024-06-06 07:32:56 +0000

    finish django registration

--- blango/settings.py
+++ blango/settings.py
@@ -181,6 +181,9 @@ class Dev(Configuration):
 
     INTERNAL_IPS = ["192.168.10.93"]
 
+    EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+    ACCOUNT_ACTIVATION_DAYS = 7
 
 
 class Prod(Dev):
